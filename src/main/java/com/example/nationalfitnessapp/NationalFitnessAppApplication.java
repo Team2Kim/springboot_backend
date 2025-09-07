fdc652f0e800c748fc95eb21662e5d136e9047f9
kim0hyeon <dbfwjs991105@gmail.com>
2025-09-07 21:05:42 +0900

    Initial commit

---
+++ src/main/java/com/example/nationalfitnessapp/NationalFitnessAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.nationalfitnessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalFitnessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalFitnessAppApplication.class, args);
+	}
+
+}
