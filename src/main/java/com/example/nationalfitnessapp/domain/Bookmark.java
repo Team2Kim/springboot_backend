3e5bcdc6b6d8b940f6aa1a2a34f51d12063a065e
kim0hyeon <dbfwjs991105@gmail.com>
2025-09-11 16:49:19 +0900

    feat: Bookmark 엔티티 추가

---
+++ src/main/java/com/example/nationalfitnessapp/domain/Bookmark.java
@@ -0,0 +1,40 @@
+package com.example.nationalfitnessapp.domain;
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Bookmark")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Bookmark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookmarkId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exerciseId", nullable = false)
+    private Exercise exercise;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    public Bookmark(User user, Exercise exercise){
+        this.user = user;
+        this.exercise = exercise;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
