b502d36e762f29b179cc52c9704089ce5ed0332d
김영현 <kimyounghyeon@gim-yeonghyeon-ui-MacBookPro.local>
2025-09-17 22:04:24 +0900

    feat: ExerciseApiResponse 클래스 구현

--- src/main/java/com/example/nationalfitnessapp/dto/FacilityApiResponse.java
+++ src/main/java/com/example/nationalfitnessapp/dto/FacilityApiResponse.java
@@ -12,9 +12,9 @@ import lombok.Setter;
 @Setter
 @XmlRootElement(name = "response")  // 최상위 태그가 <response>임을 명시
 @XmlAccessorType(XmlAccessType.FIELD)
-public class ApiResponse {
+public class FacilityApiResponse {
 
     // <body> 태그에 대한 정보를 ApiBody 객체에 매핑
     @XmlElement(name = "body")
-    private ApiBody body;
+    private FacilityApiBody body;
 }
