2b1bdd03abfcbf005ba3f1e245712907ec7b5bb4
kim0hyeon <dbfwjs991105@gmail.com>
2025-09-29 22:18:00 +0900

    feat: BookmarkResponseDto-Exercise 엔티티를 담을 수 있게 수정

--- src/main/java/com/example/nationalfitnessapp/dto/BookmarkResponseDto.java
+++ src/main/java/com/example/nationalfitnessapp/dto/BookmarkResponseDto.java
@@ -9,16 +9,12 @@ import java.time.LocalDateTime;
 public class BookmarkResponseDto {
 
     private final Long bookmarkId;
-    private final Long exerciseId;
-    private final String title;
-    private final String videoUrl;
     private final LocalDateTime createdAt;
+    private final ExerciseInBookmarkDto exercise;
 
     public BookmarkResponseDto(Bookmark bookmark){
         this.bookmarkId = bookmark.getBookmarkId();
-        this.exerciseId = bookmark.getExercise().getExerciseId();
-        this.title = bookmark.getExercise().getTitle();
-        this.videoUrl = bookmark.getExercise().getVideoUrl();
         this.createdAt = bookmark.getCreatedAt();
+        this.exercise = new ExerciseInBookmarkDto(bookmark.getExercise());
     }
 }
