a8c939d4ed4b90d0479ade45a08c9d6cf9651da5
kim0hyeon <dbfwjs991105@gmail.com>
2025-10-03 16:34:25 +0900

    feat: ExerciseController-근육 부위를 기준으로 운동 영상을 검색하고 페이징하는 API 구현

--- src/main/java/com/example/nationalfitnessapp/controller/ExerciseController.java
+++ src/main/java/com/example/nationalfitnessapp/controller/ExerciseController.java
@@ -4,7 +4,6 @@ import com.example.nationalfitnessapp.domain.Exercise;
 import com.example.nationalfitnessapp.service.ExerciseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,23 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.util.List;
+
 @RestController  // 이 클래스가 Rest API의 컨트롤러임을 나타내는 어노테이션
 @RequestMapping("/api/exercises")  // 기본 경로를 '/api/exercise' 으로 설정
 @RequiredArgsConstructor
 public class ExerciseController {
 
     private final ExerciseService exerciseService;
-
-    /**
-     * 특정 ID의 운동 영상 정보를 조회하는 API
-     * @param exerciseId URL 경로에서 받아온 ID 값
-     * @return Exercise 객체 (JSON 형태로 자동 변환되어 응답)
-     * */
-    @GetMapping("/{exerciseId}")  // Get 요청, /api/exercises/{숫자} 형태로 URL에 매핑
-    public Exercise getExerciseById(@PathVariable long exerciseId) {
-        // URL 경로의 exerciseId값을 활용해 Exercise 엔티티를 찾아 반환
-        return exerciseService.findByExerciseId(exerciseId);
-    }
 
     /**
      * 운동 영상을 검색하고 페이징하여 반환하는 API
@@ -55,5 +45,23 @@ public class ExerciseController {
 
         // 2. 서비스를 호출하여 데이터 조회
         return exerciseService.findAll(keyword, targetGroup, fitnessFactorName, exerciseTool, pageable);
+    }
+
+    /**
+     * 근육 부위를 기준으로 운동 영상을 검색하고 페이징하여 반환하는 API
+     * @param muscles 검색할 근육 이름 목록(쉼표로 구분 가능)
+     * @param page 요청할 페이지 번호 (0부터 시작)
+     * @param size 한 페이지에 보여줄 데이터 개수 (기본값 10)
+     * @return 페이징된 Exercise 데이터
+     * */
+    @GetMapping("/by-muscle")
+    public Page<Exercise> getExercisesByMuscles(
+            @RequestParam List<String> muscles,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        // 이름순으로 정렬
+        Pageable pageable = PageRequest.of(page, size, Sort.by("title").ascending());
+        return exerciseService.findExercisesByMuscles(muscles, pageable);
     }
 }
