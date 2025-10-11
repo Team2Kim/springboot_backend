373f983a3aa0b419504a24ad8556aa87dfb692d6
kim0hyeon <dbfwjs991105@gmail.com>
2025-10-11 15:18:16 +0900

    feat: DailyLogExercise-운동 기록 조회 응답 DTO 추가

---
+++ src/main/java/com/example/nationalfitnessapp/dto/DailyLogExerciseDto.java
@@ -0,0 +1,21 @@
+package com.example.nationalfitnessapp.dto;
+
+import com.example.nationalfitnessapp.domain.DailyLogExercise;
+import jakarta.persistence.Column;
+import lombok.Getter;
+
+@Getter
+public class DailyLogExerciseDto {
+
+    private final Long logExerciseId;
+    private final ExerciseResponseDto exercise;
+    private final String intensity;
+    private final int exerciseTime;
+
+    public DailyLogExerciseDto(DailyLogExercise dailyLogExercise) {
+        this.logExerciseId = dailyLogExercise.getLogExerciseId();
+        this.exercise = new ExerciseResponseDto(dailyLogExercise.getExercise());
+        this.intensity = dailyLogExercise.getIntensity();
+        this.exerciseTime = dailyLogExercise.getExerciseTime();
+    }
+}
